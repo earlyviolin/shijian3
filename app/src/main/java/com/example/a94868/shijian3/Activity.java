4c6650a0e004270236d70ef4992a15117d1fc9e3
earlyviolin <948689100@qq.com>
2017-09-15 17:10:30 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a94868/shijian3/Activity.java
@@ -0,0 +1,20 @@
+package com.example.a94868.shijian3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+
+        TextView e = (TextView)findViewById(R.id.textView);
+
+    }
+}
